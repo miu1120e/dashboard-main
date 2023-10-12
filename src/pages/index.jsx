da5c88011d247b798e04c1f43a8b290cb962ae45
ltnal34 <chae1120e@gmail.com>
2023-10-12 10:10:11 +0900

    first commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Title from '../components/title/Tilte';
+
+const Home = () => {
+  return (
+    <div>
+      <Title title ="홈" desc="이페이지는 홈입니다."/>
+    </div>
+  );
+};
+
+export default Home;
